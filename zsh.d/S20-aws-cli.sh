2dc911be94a6a56d5beb37f880f8395c61e6a1d7
Matthew Eddey <eddey@amazon.com>
2012-09-17 17:14:41 +0200

    Adding support for IAM and cloudwatch cli tools in AWS scripts

--- zsh.d/S20-aws-cli.sh
+++ zsh.d/S20-aws-cli.sh
@@ -4,6 +4,8 @@ export CLI_ROOT=$AWS_ROOT/cli
 export EC2_CLI_ROOT=$AWS_ROOT/cli/ec2
 export RDS_CLI_ROOT=$AWS_ROOT/cli/rds
 export EMR_CLI_ROOT=$AWS_ROOT/cli/emr
+export IAM_CLI_ROOT=$AWS_ROOT/cli/iam
+export CW_CLI_ROOT=$AWS_ROOT/cli/cloudwatch
 
 export AMI_ALINUX_X86_64=ami-e565ba8c
 export AMI_ALINUX_I386=ami-ed65ba84
@@ -11,14 +13,42 @@ export AMI_ALINUX_I386=ami-ed65ba84
 function use_aws_creds {
     local creds;
     creds=$CREDS_ROOT/$1
-    echo "Using AWS credentials for user '$1'"
-    export EC2_CERT=$creds/cert.pem
-    export EC2_PRIVATE_KEY=$creds/pk.pem
-    export AWS_ACCESS_KEY=`cat $creds/aws_access_key`
-    export AWS_SECRET_KEY=`cat $creds/aws_secret_key`
-    export ELASTIC_MAPREDUCE_ACCESS_ID=`cat $creds/aws_access_key`
-    export ELASTIC_MAPREDUCE_PRIVATE_KEY=`cat $creds/aws_secret_key`
+    echo "Using AWS credentials for user '$1' from $creds"
+    EC2_CERT=$creds/cert.pem
+    EC2_PRIVATE_KEY=$creds/pk.pem
+    AWS_ACCESS_KEY=`cat $creds/aws_access_key`
+    AWS_SECRET_KEY=`cat $creds/aws_secret_key`
+    ELASTIC_MAPREDUCE_ACCESS_ID=`cat $creds/aws_access_key`
+    ELASTIC_MAPREDUCE_PRIVATE_KEY=`cat $creds/aws_secret_key`
+
+    AWS_CREDENTIAL_FILE=$CREDS_ROOT/.tmp/$$-aws_credential_file
+    echo "AWSAccessKeyId=`cat $creds/aws_access_key`" > $AWS_CREDENTIAL_FILE
+    echo "AWSSecretKey=`cat $creds/aws_secret_key`" >> $AWS_CREDENTIAL_FILE
+
+    find $CREDS_ROOT/.tmp -atime +7 -delete
+
+    export EC2_CERT
+    export EC2_PRIVATE_KEY
+    export AWS_ACCESS_KEY
+    export AWS_SECRET_KEY
+    export ELASTIC_MAPREDUCE_ACCESS_ID
+    export ELASTIC_MAPREDUCE_PRIVATE_KEY
+
+    export AWS_CREDENTIAL_FILE
 }
+
+function add_aws_creds {
+    local creds;
+    creds=$CREDS_ROOT/$1
+    aws_access_key=$2
+    aws_secret_key=$3
+
+    mkdir -p $creds
+    echo ${aws_secret_key} > $creds/aws_secret_key
+    echo "$aws_access_key" > $creds/aws_access_key
+    echo "Added AWS credentials for user '$1' at $creds"
+}
+
 
 function use_ec2_api_tools {
    local newtools;
@@ -78,7 +108,48 @@ function switch_emr_api_tools {
    use_emr_api_tools $1
 }
 
+function use_iam_api_tools {
+   local newtools;
+   newtools=`readlink -f $IAM_CLI_ROOT/$1`
+   echo "Using IAM tools in '$newtools'"
+   export AWS_IAM_HOME=$newtools
+   path_prepend $AWS_IAM_HOME/bin
+}
+
+function unuse_iam_api_tools {
+   local oldtools;
+   oldtools=$AWS_IAM_HOME
+   path_remove $oldtools
+}
+
+function switch_iam_api_tools {
+   unuse_iam_api_tools
+   use_iam_api_tools $1
+}
+
+function use_cw_api_tools {
+   local newtools;
+   newtools=`readlink -f $CW_CLI_ROOT/$1`
+   echo "Using Cloudwatch tools in '$newtools'"
+   export AWS_CLOUDWATCH_HOME=$newtools
+   path_prepend $AWS_CLOUDWATCH_HOME/bin
+}
+
+function unuse_cw_api_tools {
+   local oldtools;
+   oldtools=$AWS_CLOUDWATCH_HOME
+   path_remove $oldtools/bin
+}
+
+function switch_cw_api_tools {
+   unuse_cw_api_tools
+   use_cw_api_tools $1
+}
+
+
 use_aws_creds $USER
 use_ec2_api_tools "latest"
 use_rds_api_tools "latest"
 use_emr_api_tools "latest"
+use_iam_api_tools "latest"
+use_cw_api_tools "latest"
