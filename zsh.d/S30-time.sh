bddbf88c196f632412cf209e364b4876f214952f
Matthew Eddey <eddey@amazon.com>
2013-03-14 12:19:52 +0200

    Add time reporting for long-running processes

---
+++ zsh.d/S30-time.sh
@@ -0,0 +1,4 @@
+# If a command takes >10 CPU seconds, automatically print the time it took.
+REPORTTIME=10
+# Format the output of the time command to be a little more human friendly.
+TIMEFMT=" Elapsed: %*E User: %U Kernel: %*S"
