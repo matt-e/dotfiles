fb810cfdc2a1a051fc343697fc9388779bf1f9fa
Matthew Eddey <eddey@amazon.com>
2015-12-09 10:32:36 -0800

    Remove emacs-prelude

--- .emacs.d/personal/preload/unfill.el
+++
@@ -1,8 +0,0 @@
-(defun unfill-paragraph (&optional region)
-  "Takes a multi-line paragraph and makes it into a single line of text."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max)))
-    (fill-paragraph nil region)))
-
-;; Handy key definition
-(define-key global-map "\M-Q" 'unfill-paragraph)
