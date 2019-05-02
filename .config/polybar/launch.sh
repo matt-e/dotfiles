362be0516762b5ed620cab9229c5b068eb19e63c
Matthew Eddey <matt.eddey@gmail.com>
2019-05-02 00:39:20 -0700

    All kinds of new config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env sh
 
-export PATH=$PATH:$HOME/.local/bin
+killall polybar
 
-# Terminate already running bar instances
-killall -q polybar
-
-# wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# launch polybar
-if [ "$(hostname)" = "wintermute" ]
-then
-  polybar desktop &
-else
-  if ! pgrep -x "nm-applet" >/dev/null
-  then
-    nm-applet &
-  fi
-  polybar laptop &
-fi
-
-echo "Bars launched..."
+polybar -c ~/.config/polybar/config main &
+polybar -c ~/.config/polybar/config secondary &
