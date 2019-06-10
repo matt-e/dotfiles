e5aa97ab7fd3759e65d4caccff402efd50d06b8a
Matthew Eddey <meddey@snapchat.com>
2019-06-10 09:45:12 -0700

    [emacs] Set fill column

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fill-column 120)
  '(gofmt-command "goimports")
  '(helm-adaptive-mode t nil (helm-adaptive))
  '(org-agenda-files '("~/Sync/org"))
